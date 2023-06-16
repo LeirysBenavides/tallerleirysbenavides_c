a36ca96dcaf3ccd662121e09e0e3b434f2f845e0
LeirysBenavides <136029412+LeirysBenavides@users.noreply.github.com>
2023-06-16 01:40:01 +0000

    implementacion de modulos

---
+++ programa.py
@@ -0,0 +1,10 @@
+from matematicas import sumar, restar, multiplicar, dividir
+from geometria import distancia
+
+print ("Suma:", sumar(2,3))
+print ("Resta:", restar(5,1))
+print ("Multiplicacion", multiplicar(2,2))
+print ("Division", dividir(9,3))
+
+print ("Distancia:", distancia(1,2,4,5))
+
